d74a64d84de320de625059711801c398a6b22d36
ANURAG-ARMEDU <anurag@armedu.in>
2024-08-31 21:38:53 +0530

    update index.js file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-import {app} from './app.js'
+import connectDB from "../src/db/index.js";
+import {app} from '../src/app.js'
 dotenv.config({
     path: './.env'
 })
