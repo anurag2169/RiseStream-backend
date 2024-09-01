ea9c33a0ee33de88bca912b21cc673cf0ebbf365
ANURAG-ARMEDU <anurag@armedu.in>
2024-09-01 20:19:06 +0530

    update cloudinary code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,10 +16,10 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded successfull
     // console.log("file is uploaded on cloudinary ", response.url);
-    fs.unlinkSync(localFilePath);
+    // fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    // fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
