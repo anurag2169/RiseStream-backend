71c56e508e7f50f94c9b85f056352eaba18ca992
ANURAG-ARMEDU <anurag@armedu.in>
2024-09-09 15:57:52 +0530

    update payload size

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "150mb" }));
+app.use(express.urlencoded({ extended: true, limit: "150mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
