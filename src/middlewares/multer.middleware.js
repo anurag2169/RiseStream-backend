26609867e539b0b3f69797cdd9db2f037b5b56d0
ANURAG-ARMEDU <anurag@armedu.in>
2024-09-09 16:09:57 +0530

    update multer middlerware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,15 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "/tmp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+  destination: function (req, file, cb) {
+    cb(null, "/tmp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 150 * 1024 * 1024 },
+});
